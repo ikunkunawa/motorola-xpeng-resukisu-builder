98c1ae0ec86a5b3a5965e83e153f3f534859f420
kun <kun@localhost>
2026-08-27 11:18:59 +0800

    fix-mkbootimg-gki-stub

---
+++ tools/gki/generate_gki_certificate.py
@@ -0,0 +1,4 @@
+def generate_gki_certificate(image, avbtool=None, name=None, algorithm=None,
+                              key=None, salt=None, additional_avb_args=None,
+                              output=None):
+    pass
